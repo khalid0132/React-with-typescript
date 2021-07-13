2a46161f2a8e7b76ec5aade73409b2168ab64396
Khalid Saifullah <khalid0132@gmail.com>
2021-07-13 09:45:36 +0200

    updated info

--- src/App.tsx
+++ src/App.tsx
@@ -19,22 +19,22 @@ function App() {
 
     {
       name: "Leonel Messi",
-      url: "https://imgs.aftonbladet-cdn.se/v2/images/3bad1034-be3a-4726-9b8f-5efaaca9693a?fit=crop&h=1267&q=50&w=1900&s=4a31f289637beb174d643a837dd6043d155d1ecb",
       age: 28,
+      url: "https://imgs.aftonbladet-cdn.se/v2/images/3bad1034-be3a-4726-9b8f-5efaaca9693a?fit=crop&h=1267&q=50&w=1900&s=4a31f289637beb174d643a837dd6043d155d1ecb",
       note: "Super player, try to attach in the team"
     
     },
     {
       name: "Jack Kallis",
-      url: "http://starsunfolded.com/wp-content/uploads/2018/02/JacquesKallis-07.jpg",
       age: 46,
+      url: "http://starsunfolded.com/wp-content/uploads/2018/02/JacquesKallis-07.jpg",
       note: "Super team mate, so he may join"
     
     },
     {
       name: "Sachin Tendulkar",
-      url: "https://static.toiimg.com/thumb/msid-81527421,width-1200,height-900,resizemode-4/.jpg",
       age: 42,
+      url: "https://static.toiimg.com/thumb/msid-81527421,width-1200,height-900,resizemode-4/.jpg",
       note: "Great Cricketer. Welcome to join"
     
     }
@@ -49,6 +49,7 @@ function App() {
       <h1>People invited to my Team</h1>
       <List people = {people}></List>
       <AddToList people = {people} setPeople={setPeople}></AddToList>
+      
     </div>
   );
 }
